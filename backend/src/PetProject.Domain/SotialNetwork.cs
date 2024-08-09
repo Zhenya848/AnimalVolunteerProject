5671c0840e9ec1cb40de5f5b5e86457715948afe
Zhenya848 <belovea100207@yandex.ru>
2024-08-09 10:35:46 +0300

    AddVolunteerAndSocialNetworkEntities

---
+++ backend/src/PetProject.Domain/SotialNetwork.cs
@@ -0,0 +1,8 @@
+﻿namespace PetProject.Domain
+{
+    public struct SotialNetwork
+    {
+        public string Name { get; private set; }
+        public string Reference { get; private set; }
+    }
+}
---
+++ backend/src/PetProject.Domain/Volunteer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace PetProject.Domain
+{
+    public class Volunteer
+    {
+        public Guid Id { get; private set; }
+
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public string TelephoneNumber { get; private set; }
+
+        public int EXP { get; private set; }
+        public int CountOfShelterAnimals { get; private set; }
+        public int CountOfHomelessAnimals { get; private set; }
+        public int CountOfIllAnimals { get; private set; }
+
+        public List<SotialNetwork> SotialNetworks { get; private set; }
+        public List<Requisite> Requisites { get; private set; }
+        public List<Pet> Pets { get; private set; }
+    }
+}
